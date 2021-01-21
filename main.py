651954dbc9b3096a08883df03ce17f5d241bd80a
RavenVl <ravenvl71@gmail.com>
2021-01-21 13:32:05 +0300

    2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main(filename):
 
         for i in range(5, 0, -1):
             name = f'naim{i}'
-            if record[name] !='':
+            if record[name] != '':
                 group.append(record[name])
         # group = group + ' > ' + record['naim']
         group.append(record['naim'])
@@ -18,7 +18,7 @@ def main(filename):
         print(group_txt)
 
     with open("gaz_site.csv", mode="w", encoding='utf-8') as w_file:
-        file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+        file_writer = csv.writer(w_file, delimiter=",", escapechar=' ', quoting=csv.QUOTE_NONE)
         for record in _dbf:
             group = []
 
@@ -30,7 +30,9 @@ def main(filename):
             group.append(record['naim'])
             group_txt = ' > '.join(group)
             print(group_txt)
-            file_writer.writerow(['Газю.об.', group_txt, record['kodpr'], record['tovar'], '','','Gaz partner', record['cena'], record['kol'] ])
+            file_writer.writerow(
+                ['Газю.об.', group_txt, record['kodpr'], record['tovar'], '', '', 'Gaz partner', record['cena'],
+                 record['kol']])
 
 
 if __name__ == '__main__':
